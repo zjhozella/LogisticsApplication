cd1af1ed8e2dd2651b5c919ec33ccd1a585ad8c9
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-10-05 13:26:17 -0400

    r

--- LogisticsApplication/src/model/Load.java
+++ LogisticsApplication/src/model/Load.java
@@ -11,24 +11,72 @@ package model;
  */
 public class Load {
     // Default constructor
-    int truckNumber   = 0;
-    int trailerNumber = 0;
-    int storeNumber   = 0;
-    int sealNumber    = 0;
-    int driverNumber  = 0;
-    boolean loadComplete = false;
+    private int truckNumber   = 0;
+    private int trailerNumber = 0;
+    private int storeNumber   = 0;
+    private int sealNumber    = 0;
+    private int driverNumber  = 0;
+    private boolean loadComplete = false;
     //Need to input timestamps
 
 
     // Constructor for load class
-    public Load (int truckNumber, int trailerNumber, int storeNumber, int sealNumber, int driverNumber, boolean loadComplete){
-        this.trailerNumber   = newTrailerNumber;
-        this.truckNumber     = newTruckNumber;
-        this.storeNumber     = newStoreNumber;
-        this.sealNumber      = newSealNumber;
-        this.driverNumber    = newDriverNumber;
-        this.loadComplete    = newLoadComplete;
+    public Load (int newTrailerNumber, int newTruckNumber, int newStoreNumber , int newSealNumber , int newDriverNumber , boolean newLoadComplete ){
+        this.setTrailerNumber(newTrailerNumber);
+        this.setTruckNumber(newTruckNumber);
+        this.setStoreNumber(newStoreNumber);
+        this.setSealNumber(newSealNumber);
+        this.setDriverNumber(newDriverNumber);
+        this.setLoadComplete(newLoadComplete);
 
 
+    }
+
+    public int getTruckNumber() {
+        return truckNumber;
+    }
+
+    public void setTruckNumber(int truckNumber) {
+        this.truckNumber = truckNumber;
+    }
+
+    public int getTrailerNumber() {
+        return trailerNumber;
+    }
+
+    public void setTrailerNumber(int trailerNumber) {
+        this.trailerNumber = trailerNumber;
+    }
+
+    public int getStoreNumber() {
+        return storeNumber;
+    }
+
+    public void setStoreNumber(int storeNumber) {
+        this.storeNumber = storeNumber;
+    }
+
+    public int getSealNumber() {
+        return sealNumber;
+    }
+
+    public void setSealNumber(int sealNumber) {
+        this.sealNumber = sealNumber;
+    }
+
+    public int getDriverNumber() {
+        return driverNumber;
+    }
+
+    public void setDriverNumber(int driverNumber) {
+        this.driverNumber = driverNumber;
+    }
+
+    public boolean isLoadComplete() {
+        return loadComplete;
+    }
+
+    public void setLoadComplete(boolean loadComplete) {
+        this.loadComplete = loadComplete;
     }
 }
